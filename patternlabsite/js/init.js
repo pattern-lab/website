dea0cbe0f552de5807b3e3d61c7652c2607d03f7
Ian Frost <inf5000@gmail.com>
2015-10-16 18:45:15 -0400

    Created tab toggle with active class and displayed each when active

--- patternlabsite/js/init.js
+++ patternlabsite/js/init.js
@@ -1,3 +1,40 @@
-(function(w){
-	var nav = responsiveNav(".nav", { insert: "before" });
-})(this);
+/* var languageBtn = document.querySelectorAll(".tab");
+for (i=0; i<languageBtn.length; ++i){
+	console.log(languageBtn[i]);
+	languageBtn.addEventListener("click", function(event){
+		console.log(languageBtn);
+		event.preventDefault();
+		//this.className.add("active");
+		//if (languageBtn[i].className === "active") {
+		//this.className.remove("active");
+		//}
+		//else {
+		//this.classList.add("active");
+		//alert(i);
+		//}
+	});
+} */
+
+
+var languageBtn = $('.tab');
+$('.tab:nth-of-type(1)').addClass('active');
+languageBtn.on('click', function(e){ 
+	e.preventDefault();
+	languageBtn.removeClass('active');
+	$(this).addClass('active');
+	var thisHref = $('.tab.active .language-btn').attr('href');
+	console.log(thisHref);
+	if (thisHref === '#php') {	
+	$('#node').css('display', 'none');
+	$('#php').css('display', 'block');
+	}
+	else if (thisHref === '#node') {
+		$('#php').css('display', 'none');
+		$('#node').css('display', 'block')
+	}
+	
+});
+
+
+//$('.php-block').css('background', 'red');
+//$('.node-block').css('background', 'blue');
