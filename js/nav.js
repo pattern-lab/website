ad9d36eca4fe3c3e8e8d0f7843753a42b046abf5
Brad Frost <brad@bradfrost.com>
2016-06-17 10:52:56 -0400

    New design

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,15 @@
-var navActive = false;
-var navOpen = document.querySelector('.nav-link');
-var navListOpen = document.querySelector('.nav-list');
+var navActive = false,
+    navOpen = document.querySelector('.nav__btn'),
+    navListOpen = document.querySelector('.nav__list');
+
 navOpen.addEventListener("click", function(event){
 	event.preventDefault();
-	if (navActive === false) {
-	navActive = true;	
-	 navListOpen.classList.add("active");
-	 }
-	 else {
+
+    if (navActive === false) {
+	    navActive = true;
+	    navListOpen.classList.add("active");
+	} else {
 		 navActive = false;
 		 navListOpen.classList.remove("active");
-	 }
-	});
+	}
+});
