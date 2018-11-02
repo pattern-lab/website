b9bb64b3c139a68adad21fb5c056d47e39a18d6a
Ian Frost <inf5000@gmail.com>
2018-11-02 17:03:24 -0400

    Fixed JS bug on the homepage

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,17 @@
-var navActive = false,
-    navOpen = document.querySelector('.nav__btn'),
-    navListOpen = document.querySelector('.nav__list');
+if (document.querySelector('.nav__btn')) {
+    var navActive = false,
+        navOpen = document.querySelector('.nav__btn'),
+        navListOpen = document.querySelector('.nav__list');
 
-navOpen.addEventListener("click", function(event){
-	event.preventDefault();
+    navOpen.addEventListener("click", function(event){
+    	event.preventDefault();
 
-    if (navActive === false) {
-	    navActive = true;
-	    navListOpen.classList.add("nav__list--is-active");
-	} else {
-		 navActive = false;
-		 navListOpen.classList.remove("nav__list--is-active");
-	}
-});
+        if (navActive === false) {
+    	    navActive = true;
+    	    navListOpen.classList.add("nav__list--is-active");
+    	} else {
+    		 navActive = false;
+    		 navListOpen.classList.remove("nav__list--is-active");
+    	}
+    });
+}
