fb40ca5f0ad8b4f9580c71f4bc0c562e7dfb0409
Josef Engelfrost <josef@josf.se>
2019-01-04 00:36:49 +0100

    Cache the PatternEngine list & version (forever)

--- js/pattern-engines.js
+++ js/pattern-engines.js
@@ -1,34 +1,68 @@
 'use strict';
 
 // We use the new fetch API and promises. The fallback is to link to a list in the monorepo.
-if (window.fetch !== undefined && typeof Promise !== "undefined") {
+if (window.fetch !== undefined && typeof Promise !== "undefined" && window.localStorage !== undefined) {
+  var localStorageSetItem = function (key, value) {
+    window.localStorage.setItem(key, value);
+  }
+
+  try {
+    // Safari will throw an error in Private Browsing mode if we try to save to LocalStorage
+    window.localStorage.setItem('test', true);
+    window.localStorage.removeItem('test');
+  } catch (e) {
+    localStorageSetItem = function () {};
+  }
+
   (function () {
     var repoPackagesUrl = 'https://api.github.com/repos/pattern-lab/patternlab-node/contents/packages?ref=master';
     var $list = $('#pattern-engine-list');
 
     function getPatternEngineVersion(packagePath) {
-      var packageJsonUrl = 'https://api.github.com/repos/pattern-lab/patternlab-node/contents/<packagePath>/package.json?ref=master'
+      var packageJsonUrl = 'https://api.github.com/repos/pattern-lab/patternlab-node/contents/<packagePath>/package.json?ref=master';
+      var cachedVersion = window.localStorage.getItem(packagePath + '::version');
 
+      if (cachedVersion !== null) {
+        return Promise.resolve(cachedVersion);
+      }
+
+      // Fetch the version if it's not cached
       return window.fetch(packageJsonUrl.replace('<packagePath>', packagePath), { mode: 'cors' })
         .then(function (response) {
           return response.json();
         }).then(function (responseJson) {
-          return JSON.parse(atob(responseJson.content)).version;
+          var version = JSON.parse(atob(responseJson.content)).version;
+
+          // Cache the version
+          localStorageSetItem(packagePath + '::version', version);
+          return version;
         });
     }
 
     $list.html('<li>Loading...</li>');
 
-    // Use the GitHub API to get a list of all the packages in the monorepo
-    window.fetch(repoPackagesUrl, { mode: 'cors' }).then(function (response) {
-      return response.json();
-    }).then(function (responseJson) {
-      $list.html('');
-//function(a, b) { return a.path === b.path ? 0 : a.path > b.path ? 1 : -1 }
-      responseJson.sort().forEach(function (repoPackage) {
-        // Show a list of all the packages starting with `engine-`
-        if (repoPackage.path.indexOf('packages/engine-') === 0) {
+    var cachedPackageList = JSON.parse(window.localStorage.getItem('packages'));
+    var packageListPromise;
 
+    if (cachedPackageList !== null) {
+      packageListPromise = Promise.resolve(cachedPackageList);
+    } else {
+      packageListPromise = window.fetch(repoPackagesUrl, { mode: 'cors' })
+        .then(function (response) {
+          return response.json();
+        }).then(function (packageList) {
+          localStorageSetItem('packages', JSON.stringify(packageList));
+          return packageList;
+        });
+    }
+
+    // Use the GitHub API to get a list of all the packages in the monorepo
+    packageListPromise.then(function (packageList) {
+      $list.html('');
+      packageList.sort().forEach(function (repoPackage) {
+
+        // All the PatternEngine packages start with `engine-`
+        if (repoPackage.path.indexOf('packages/engine-') === 0) {
           var engineName = repoPackage.path.replace('packages/engine-', '');
           $list.append($(
             '<li>'
